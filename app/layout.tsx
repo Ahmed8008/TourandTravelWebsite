b6312e8533256ccc46bfd0f58f92c18126f6d266
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2025-05-15 19:01:05 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import MainNav from "@/components/Home/Navbar/MainNav";
-
-
 
 const font = Poppins({
   weight:['100','200','300','400','500','600','700','800','900'],
   subsets:['latin']
 })
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Travel for you | Next js",
